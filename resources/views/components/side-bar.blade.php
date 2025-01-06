e44dcf6e1f9f58c774114878eb56208b87f6be98
silasrai <silasraii144@gmail.com>
2025-01-06 08:46:20 +0545

    project controller setup

--- resources/views/components/side-bar.blade.php
+++ resources/views/components/side-bar.blade.php
@@ -8,9 +8,9 @@
     <!-- End Dashboard Nav -->
     <li class="nav-heading">Pages</li>
     <li class="nav-item">
-        <a class="nav-link collapsed" href="users-profile.html">
+        <a class="nav-link collapsed" href="">
             <i class="bi bi-person"></i>
-            <span>Profile</span>
+            <span>Project</span>
         </a>
     </li>
     <!-- End Profile Page Nav -->
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\admin\DashboardController;
+use App\Http\Controllers\admin\ProjectController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\frontend\PageController;
 use Illuminate\Support\Facades\Route;
@@ -11,3 +12,4 @@ Route::post('/login',[AuthController::class,'login_post'])->name('login_post');
 Route::get('/register',[AuthController::class,'register'])->name('register');
 Route::post('/register',[AuthController::class,'register_post'])->name('register_post');
 Route::get('admin/dashboard',[DashboardController::class,'dashboard'])->name('dashboard');
+Route::resource('admin/project',ProjectController::class)->names('project');
